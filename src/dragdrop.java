1cd000d93c3e090169ef9da8eb9109f17780f597
manishh0906 <153229159+manishh0906@users.noreply.github.com>
2024-04-24 10:29:52 +0530

    change

--- src/dragdrop.java
+++ src/dragdrop.java
@@ -7,18 +7,19 @@ import org.openqa.selenium.interactions.Actions;
 public class dragdrop {
 
 	public static void main(String[] args) throws InterruptedException {
-		System.setProperty("webdriver.chrome.driver", "C:\\Users\\manishwrq\\Downloads\\Compressed\\chromedriver-win64\\chromedriver-win64\\chromedriver.exe");
-	WebDriver driver = new ChromeDriver();
-	driver.get("https://www.globalsqa.com/demo-site/draganddrop/#Photo%20Manager");
-	Thread.sleep(5000);
-	driver.manage().window().maximize();
-	driver.switchTo().frame(driver.findElement(By.xpath("//iframe[@class='demo-frame lazyloaded']")));
-	Actions act = new Actions(driver);		// TODO Auto-generated method stub
-	 WebElement drag = driver.findElement(By.xpath("//img[@alt='The peaks of High Tatras']"));
-     WebElement drop = driver.findElement(By.xpath("//div[@id='trash']"));
-     act.dragAndDrop(drag, drop).perform();
-     
-     driver.quit();
+		System.setProperty("webdriver.chrome.driver",
+				"C:\\Users\\manishwrq\\Downloads\\Compressed\\chromedriver-win64\\chromedriver-win64\\chromedriver.exe");
+		WebDriver driver = new ChromeDriver();
+		driver.get("https://www.globalsqa.com/demo-site/draganddrop/#Photo%20Manager");
+		Thread.sleep(8000);
+		driver.manage().window().maximize();
+		driver.switchTo().frame(driver.findElement(By.xpath("//iframe[@class='demo-frame lazyloaded']")));
+		Actions act = new Actions(driver); // TODO Auto-generated method stub
+		WebElement drag = driver.findElement(By.xpath("//img[@alt='The peaks of High Tatras']"));
+		WebElement drop = driver.findElement(By.xpath("//div[@id='trash']"));
+		act.dragAndDrop(drag, drop).perform();
+
+		driver.quit();
 	}
 
 }
